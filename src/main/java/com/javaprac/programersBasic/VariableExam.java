bfc9dce3653bceb46f665f5dedc358cc1cf73699
bigduk88 <hangdeuk@naver.com>
2021-06-23 06:55:33 +0900

    변수

---
+++ src/main/java/com/javaprac/programersBasic/VariableExam.java
@@ -0,0 +1,19 @@
+package com.javaprac.programersBasic;
+
+public class VariableExam {
+    public static void main(String[] args) {
+        int count;
+
+        count = 10;
+        count = 20;
+
+        System.out.println(count);
+
+        double avg = 11.1;
+
+        String name = "carami";
+        //변수는 첫 번째 문자가 소문자.
+
+        int totalCount;
+    }
+}
