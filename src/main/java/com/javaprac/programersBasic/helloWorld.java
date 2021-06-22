68bd86070a2ebb1b507b1aa0178c9f8407e76d33
bigduk88 <hangdeuk@naver.com>
2021-06-23 06:48:11 +0900

    주석문

--- src/main/java/com/javaprac/programersBasic/helloWorld.java
+++ src/main/java/com/javaprac/programersBasic/helloWorld.java
@@ -1,7 +1,21 @@
 package com.javaprac.programersBasic;
 
 public class helloWorld {
+    // 프로그램의 시작점. 구현주석. 단축키 커맨드 /
+
+    /**
+     *
+     * @param args 문서화 주석
+     */
+
+    /*
+    블럭단위 주석.
+     */
     public static void main(String[] args) {
         System.out.println("hello world");
+//        System.out.println("test");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 }
