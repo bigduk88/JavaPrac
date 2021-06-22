abaf65ef0d0c9bebd5d5b8e5d81fb02bd7715f86
bigduk88 <hangdeuk@naver.com>
2021-06-23 07:12:58 +0900

    기본형 데이터 타입

---
+++ src/main/java/com/javaprac/programersBasic/PrimitiveDataTypeExam.java
@@ -0,0 +1,19 @@
+package com.javaprac.programersBasic;
+
+public class PrimitiveDataTypeExam {
+    public static void main(String[] args) {
+        boolean isFun = true;
+        System.out.println(isFun);
+
+        char c = 'f';
+        //문자 하나만 표현 가능
+
+        int x = 59;
+
+        long bing = 723232303L;
+
+        float f = 32.4f;
+
+        double d = 849345934853.3;
+    }
+}
