0801e4e24d66d19f574fdf2aad06d1f698fb1305
bigduk88 <hangdeuk@naver.com>
2021-06-24 07:38:27 +0900

    [2021.06.24] 클래스

---
+++ src/main/java/com/javaprac/programersBasic/Car.java
@@ -0,0 +1,5 @@
+package com.javaprac.programersBasic;
+
+public class Car {
+
+}
---
+++ src/main/java/com/javaprac/programersBasic/CarExam.java
@@ -0,0 +1,9 @@
+package com.javaprac.programersBasic;
+
+public class CarExam {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+    }
+}
