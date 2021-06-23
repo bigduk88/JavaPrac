dae0f81e3a4740af6df6af1f4be72774f3ef441e
bigduk88 <hangdeuk@naver.com>
2021-06-24 07:31:50 +0900

    [2021.06.24] for each

---
+++ src/main/java/com/javaprac/programersBasic/forEachExam.java
@@ -0,0 +1,16 @@
+package com.javaprac.programersBasic;
+
+public class forEachExam {
+    public static void main(String[] args) {
+        int[] iarr = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < iarr.length; i++) {
+            int value = iarr[i];
+            System.out.println(value);
+        }
+        // for each for(타입 값을 받아줄 수 있는 변수 명 : 출력하고 싶은 자료구조)
+        for (int value : iarr) {
+            System.out.println(value);
+        }
+    }
+}
