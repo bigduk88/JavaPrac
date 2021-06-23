4f17d88b4f8c35b9de3a7b1516fc67c2c7473113
bigduk88 <hangdeuk@naver.com>
2021-06-24 07:42:46 +0900

    [2021.06.24] 참조형 변수

---
+++ src/main/java/com/javaprac/programersBasic/ReferenceDataType.java
@@ -0,0 +1,10 @@
+package com.javaprac.programersBasic;
+
+public class ReferenceDataType {
+    public static void main(String[] args) {
+        int i = 4;
+
+        String str = new String("hello");
+
+    }
+}
