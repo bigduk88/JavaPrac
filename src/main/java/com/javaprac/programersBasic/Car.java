8a4b5e93034b80ebef442333ecc11128aedaa25a
bigduk88 <hangdeuk@naver.com>
2021-06-25 07:45:30 +0900

    [2021.06.25] 생성자 overloading과 this생성자

--- src/main/java/com/javaprac/programersBasic/Car.java
+++ src/main/java/com/javaprac/programersBasic/Car.java
@@ -9,4 +9,15 @@ public class Car {
     public Car(String nanme) {
         this.name = name;
     }
+
+    public Car() {
+//        this.name = "이름 없음";
+//        this.number = 0;
+        this("이름없음", 0);
+    }
+
+    public Car(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
 }
