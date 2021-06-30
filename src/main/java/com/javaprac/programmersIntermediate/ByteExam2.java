77cc676ba7b72c0550c51d4ee6a3df80552642ed
bigduk88 <hangdeuk@naver.com>
2021-07-01 07:28:26 +0900

    [2021.07.01] char 단위 입출력(Console) 까지 실습

--- src/main/java/com/javaprac/programmersIntermediate/ByteExam2.java
+++ src/main/java/com/javaprac/programmersIntermediate/ByteExam2.java
@@ -1,11 +1,10 @@
 package com.javaprac.programmersIntermediate;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class ByteExam1 {
+public class ByteExam2 {
     public static void main(String[] args) {
         FileInputStream fis = null;
         FileOutputStream fos = null;
---
+++ src/main/java/com/javaprac/programmersIntermediate/ByteExam3.java
@@ -0,0 +1,20 @@
+package com.javaprac.programmersIntermediate;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class ByteExam3 {
+    public static void main(String[] args) {
+        try (
+                DataOutputStream out = new DataOutputStream(new FileOutputStream("data.txt"));
+        ) {
+            out.write(100);
+            out.writeBoolean(true);
+            out.writeDouble(50.5);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
